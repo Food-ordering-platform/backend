e87406b5fcfa5045e7c317d306be774a17192076
therealEmma <eozegbe68@gmail.com>
2025-09-11 20:47:29 +0100

    trying to host backend

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const PORT  = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on https://localhost:${PORT}`)
+    console.log(`Server running on http://localhost:${PORT}`)
 })

de5d7be9d27eff3d3981d0be9fbb0c43be7746d6
therealEmma <eozegbe68@gmail.com>
2025-09-24 19:52:26 +0100

    Email service on vercel

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -3,17 +3,21 @@ import axios from "axios";
 export async function sendOtPEmail(email: string, otp: string) {
   try {
     const response = await axios.post(
-      process.env.EMAIL_SERVICE_URL!, // Render email service URL
+      process.env.EMAIL_SERVICE_URL!, // from .env
       {
         to: email,
         subject: "Your OTP Code",
         html: `<p>Your OTP code is: <b>${otp}</b></p>`,
       }
     );
+
     console.log("OTP email sent successfully:", response.data);
     return response.data;
   } catch (err: any) {
-    console.error("Error sending OTP email:", err.response?.data || err.message);
+    console.error(
+      "Error sending OTP email:",
+      err.response?.data || err.message
+    );
     throw new Error("OTP email failed");
   }
 }

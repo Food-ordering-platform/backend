6f2628d95109b1eccd3aa34c8cda785da9a24f34
therealEmma <eozegbe68@gmail.com>
2025-12-25 21:50:21 +0100

    Fixed backend server issues

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
-import express from "express";
 import http from "http";
-import { initSocket } from "./utils/socket"; 
+import app from "./app"; 
+import { initSocket } from "./utils/socket";
 
+const server = http.createServer(app); // Wrap the existing app
+const io = initSocket(server);
 
-const app = express();
-const server = http.createServer(app); 
-const io = initSocket(server); 
+const PORT = process.env.PORT || 4000;
 
-
-const PORT = process.env.PORT || 5000;
-
-// CHANGE app.listen TO server.listen
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
